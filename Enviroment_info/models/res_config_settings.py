3515ed9d162ef48cd33f2cebd02c205533e54445
EmanueleZii <241270998+emaZii@users.noreply.github.com>
2026-03-06 12:43:39 +0100

    Update res_config_settings.py

--- Enviroment_info/models/res_config_settings.py
+++ Enviroment_info/models/res_config_settings.py
@@ -16,7 +16,7 @@ class EnvironmentBanner(models.Model):
     @api.model
     def log_enviroment(self):
         stage = os.environ.get('ODOO_STAGE', 'NON IMPOSTATA')
-        branch = os.environ.get('GITHUB_BRANCH', 'NON IMPOSTATA')
+        #branch = os.environ.get('GITHUB_BRANCH', 'NON IMPOSTATA')
         _logger.info("=== ENVIRONMENT BANNER === ODOO_STAGE = '%s'", stage)
-        _logger.info("=== ENVIRONMENT BANNER === GITHUB_BRANCH = '%s'", branch)
+        #_logger.info("=== ENVIRONMENT BANNER === GITHUB_BRANCH = '%s'", branch)
             
